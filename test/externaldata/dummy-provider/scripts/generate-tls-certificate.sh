0ecefa20bf18ed0e1680d42f40c47fc0e81dbe42
Yingrui Tong <yingruitong@microsoft.com>
2025-06-09 06:56:40 +0000

    change to approvals validator

--- test/externaldata/dummy-provider/scripts/generate-tls-certificate.sh
+++ test/externaldata/dummy-provider/scripts/generate-tls-certificate.sh
@@ -8,15 +8,15 @@ GATEKEEPER_NAMESPACE=${GATEKEEPER_NAMESPACE:-gatekeeper-system}
 
 generate() {
     # generate CA key and certificate
-    echo "Generating CA key and certificate for dummy provider..."
+    echo "Generating CA key and certificate for approvals provider..."
     openssl genrsa -out ca.key 2048
     openssl req -new -x509 -days 365 -key ca.key -subj "/O=Gatekeeper/CN=Gatekeeper Root CA" -out ca.crt
 
     # generate server key and certificate
-    echo "Generating server key and certificate for dummy provider..."
+    echo "Generating server key and certificate for approvals provider..."
     openssl genrsa -out server.key 2048
-    openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/CN=dummy-provider.${GATEKEEPER_NAMESPACE}" -out server.csr
-    openssl x509 -req -extfile <(printf "subjectAltName=DNS:dummy-provider.${GATEKEEPER_NAMESPACE}") -days 1 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
+    openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/CN=approvals-validator.${GATEKEEPER_NAMESPACE}" -out server.csr
+    openssl x509 -req -extfile <(printf "subjectAltName=DNS:approvals-validator.${GATEKEEPER_NAMESPACE}") -days 1 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
 }
 
 mkdir -p "/home/graytownuser/gatekeeper/gatekeeper/test/externaldata/dummy-provider/certs"
